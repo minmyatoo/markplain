bdeb62e15ad3407c1ccbdfbffc00adf482ec6543
Min Myat Oo <42258478+minmyatoo@users.noreply.github.com>
2023-12-17 03:57:46 +0800

    fix(cdn):update with CDN link

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('node_modules/bootstrap/dist'));
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -26,7 +26,7 @@ app.post('/convert', (req, res) => {
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link rel="stylesheet" href="/css/bootstrap.min.css">
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
             <title>Markdown to Plain Text Converter</title>
         </head>
         <body class="container mt-5">
@@ -56,7 +56,7 @@ app.post('/convert', (req, res) => {
             </div>
 
             <script src="https://unpkg.com/bootstrap-icons@1.19.0/font/bootstrap-icons.css"></script>
-            <script src="/js/bootstrap.bundle.min.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
              <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> <!-- Include SweetAlert -->
             <script>
                 document.getElementById('copyButton').addEventListener('click', function () {
